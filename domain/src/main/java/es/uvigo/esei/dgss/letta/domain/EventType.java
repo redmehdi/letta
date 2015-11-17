0a78d3856376e9642da6eb761d1cee79d91c0e92
aalopez <aalopez@esei.uvigo.es>
2015-11-17 18:32:47 +0100

    Modifies language of EventType enum
    Changes the language of EventType.java from Spanish to English.

--- domain/src/main/java/es/uvigo/esei/dgss/letta/domain/EventType.java
+++ domain/src/main/java/es/uvigo/esei/dgss/letta/domain/EventType.java
@@ -1,5 +1,5 @@
 package es.uvigo.esei.dgss.letta.domain;
 
 public enum EventType {
-	LITERATURA, MUSICA, CINE, TV, DEPORTES, INTERNET, VIAJES, TEATRO
+	LITERATURE, MUSIC, CINEMA, TV, SPORTS, INTERNET, TRAVELS, THEATRE
 }
