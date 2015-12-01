ca6ff60380daec7fecd0bc05aec9d9e42e044a59
aalopez <aalopez@esei.uvigo.es>
2015-12-01 13:24:13 +0100

    Adds tests for EventEjb
    Implements tests for the method registerToEvent
    Modifies EventAlreadyJoinedException

--- service/src/main/java/es/uvigo/esei/dgss/letta/service/util/exceptions/EventAlredyJoinedException.java
+++ service/src/main/java/es/uvigo/esei/dgss/letta/service/util/exceptions/EventAlredyJoinedException.java
@@ -6,30 +6,13 @@ import es.uvigo.esei.dgss.letta.domain.entities.User;
 import static java.text.MessageFormat.format;
 import static java.util.Objects.requireNonNull;
 
-public final class EventAlredyJoinedException extends Exception {
+public class EventAlredyJoinedException extends Exception {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private final User  user;
-    private final Event event;
-
-    public EventAlredyJoinedException(final User user, final Event event) {
-        super(format(
-            "User '{0}' already registered for the Event with ID '{1}'",
-            requireNonNull(user).getLogin(),
-            requireNonNull(event).getId()
-        ));
-
-        this.user  = user;
-        this.event = event;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public Event getEvent() {
-        return event;
-    }
+	public EventAlredyJoinedException(final User user, final Event event) {
+		super(format("User '{0}' already registered for the Event with ID '{1}'", requireNonNull(user).getLogin(),
+				requireNonNull(event).getId()));
+	}
 
 }
