4938ed36b399642b1083790a738a85834d8e118d
Miguel Reboiro-Jato <mrjato@esei.uvigo.es>
2015-12-03 13:11:17 +0100

    Fixes the message generation in the EventAlreadyJoinedException class
    The message in the EventAlreadyJoinedException was intended to contain
    the user's login and the message's id, but it wasn't. This commit fixes
    this problem, changing the formatting method employed.

--- service/src/main/java/es/uvigo/esei/dgss/letta/service/util/exceptions/EventAlredyJoinedException.java
+++ service/src/main/java/es/uvigo/esei/dgss/letta/service/util/exceptions/EventAlredyJoinedException.java
@@ -1,18 +1,18 @@
 package es.uvigo.esei.dgss.letta.service.util.exceptions;
 
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+
 import es.uvigo.esei.dgss.letta.domain.entities.Event;
 import es.uvigo.esei.dgss.letta.domain.entities.User;
 
-import static java.text.MessageFormat.format;
-import static java.util.Objects.requireNonNull;
-
 public class EventAlredyJoinedException extends Exception {
-
 	private static final long serialVersionUID = 1L;
 
 	public EventAlredyJoinedException(final User user, final Event event) {
-		super(format("User '{0}' already registered for the Event with ID '{1}'", requireNonNull(user).getLogin(),
-				requireNonNull(event).getId()));
+		super(format("User '%s' already registered for the Event with ID '%d'",
+			requireNonNull(user).getLogin(),
+			requireNonNull(event).getId()
+		));
 	}
-
 }
