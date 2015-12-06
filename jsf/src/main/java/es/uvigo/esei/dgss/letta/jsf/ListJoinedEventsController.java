dffa42f8dba503458829e39515b5ab983c7669c2
Borja Cordeiro Gonzalez <bcgonzalez3@esei.uvigo.es>
2015-12-06 20:40:03 +0100

    Adds jsf view and controller to list joined events
    Added a view to list the events joined by an user(listJoinedEvent.xhtml).
    Added controller with pagination to list joined events(Both links and button pagination).
    Updated menu in templateHeader to show the options of a logged in user.
    Uncommented a line in templateHeader to allow the use of Jquery

--- jsf/src/main/java/es/uvigo/esei/dgss/letta/jsf/ListJoinedEventsController.java
+++ jsf/src/main/java/es/uvigo/esei/dgss/letta/jsf/ListJoinedEventsController.java
@@ -1,10 +1,13 @@
 package es.uvigo.esei.dgss.letta.jsf;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Inject;
 
 import es.uvigo.esei.dgss.letta.domain.entities.Event;
@@ -20,7 +23,7 @@ import es.uvigo.esei.dgss.letta.service.EventEJB;
  * @author Adrian Rodriguez Fariña
  *
  */
-@RequestScoped
+@SessionScoped
 @ManagedBean(name = "listJoinedEventsController")
 public class ListJoinedEventsController{
 
@@ -33,6 +36,7 @@ public class ListJoinedEventsController{
 	private int pageIndex=1;
     private int pages=0;
     private final int CTE_NUM_EVENTS_PAGE=4;
+	private ArrayList<String> pagesLinks = new ArrayList<>();
     
 	public int getPages() {
 		return pages;
@@ -46,8 +50,13 @@ public class ListJoinedEventsController{
 	public void setPageIndex(int pageIndex) {
 		this.pageIndex = pageIndex;
 	}
-	
-    /**
+    public ArrayList<String> getPagesLinks() {
+		return pagesLinks;
+	}
+	public void setPagesLinks(ArrayList<String> pagesLinks) {
+		this.pagesLinks = pagesLinks;
+	}
+	/**
      * Initialize the number of pages in a paginated page.
      *
      */
@@ -56,18 +65,23 @@ public class ListJoinedEventsController{
     	pages=eventEJB.getCountEventsJoinedByUser()/CTE_NUM_EVENTS_PAGE;
     	if (eventEJB.getCountEventsJoinedByUser()%CTE_NUM_EVENTS_PAGE != 0)
     		pages++;
+		for (int i = 0; i < pages; i++) {
+			pagesLinks.add(String.valueOf(i + 1));
+		}
     }
 	
     /**
      * Retrieves a {@link List} of {@link Event}s from
      * {@link EventEJB#getCountEventsJoinedByUser()}, sorted by desccending date and
-     * ascending tittle.
+     * ascending title.
      *
      * @return an ordered List of twenty events.
      */
     public List<Event> getEventList() {
-        return eventEJB.getEventsJoinedByUser((this.pageIndex-1)*CTE_NUM_EVENTS_PAGE, CTE_NUM_EVENTS_PAGE);
-    }
+		return eventEJB.getEventsJoinedByUser(
+				(this.pageIndex - 1) * CTE_NUM_EVENTS_PAGE,
+				CTE_NUM_EVENTS_PAGE);
+	}
     
 
     /**
@@ -101,5 +115,21 @@ public class ListJoinedEventsController{
     public String getIconFor(final EventType eventType) {
         return eventMapper.getIconFor(eventType);
     }
+    
+    /**
+     * Update the value of pageIndex and redirect.
+     *
+     * @param pageNumber, number of page to 
+     *
+     */
+	public void jumpToPage(String pageNumber) {
+		this.pageIndex = Integer.parseInt(pageNumber);
+	}
 	
+	public boolean isAfter(final Date d) {
+		Date today = new Date();
+		if (today.after(d))
+			return true;
+		return false;
+	}
 }
