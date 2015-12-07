9a04e7fba278a46e1eba9a37ea62d168bbea78f2
Alberto G. Jácome <agjacome@gmail.com>
2015-12-07 01:43:41 +0100

    tsk27 Adds API methods to search and list highlighted events
    REST resource for Events has been updated with new methods to list currently
    highlighted events and to allow performing searches of events by matching
    search terms in their titles and summaries.

--- rest/src/main/java/es/uvigo/esei/dgss/letta/rest/EventResource.java
+++ rest/src/main/java/es/uvigo/esei/dgss/letta/rest/EventResource.java
@@ -9,12 +9,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import es.uvigo.esei.dgss.letta.domain.entities.Event;
 import es.uvigo.esei.dgss.letta.service.EventEJB;
 
 import static javax.ws.rs.core.Response.status;
+import static javax.ws.rs.core.Response.Status.OK;
 
 import static org.apache.commons.lang3.Validate.isTrue;
 
@@ -43,17 +43,67 @@ public class EventResource {
      *
      * @return An {@code OK} HTTP response containing the list of events in the
      *         requested page.
+     *
+     * @throws IllegalArgumentException If either the page number is less than
+     *         one or the page size is less than zero.
      */
     @GET
     public Response list(
         @QueryParam("page") @DefaultValue("1")  final int page,
         @QueryParam("size") @DefaultValue("20") final int size
-    ) {
+    ) throws IllegalArgumentException {
         isTrue(page >= 1, "Page number must be greater than zero");
         isTrue(size >= 0, "Page size must be non-negative");
 
         final int start = (page - 1) * size;
-        return status(Status.OK).entity(events.listByDate(start, size)).build();
+        return status(OK).entity(events.listByDate(start, size)).build();
+    }
+
+    /**
+     * Returns the list of {@link Event Events} that are currently highlighted.
+     *
+     * @return A {@code OK} HTTP response containing the list of the events in
+     *         the application that are currently highlighted.
+     */
+    @GET
+    @Path("highlighted")
+    public Response highlighted() {
+        return status(OK).entity(events.listHighlighted()).build();
+    }
+
+    /**
+     * Returns the list of {@link Event Events} matching a given search query.
+     * The search will be performed in both the event's {@link Event#getTitle()
+     * title} and the {@link Event#getShortDescription() summary}.
+     *
+     * @param query The search query (title/summary terms) to perform. If not
+     *        set, it will be defaulted to an empty search query, thus
+     *        supposedly returning all the events (equivalent to
+     *        {@link #list(int, int)}).
+     * @param page Page number to retrieve, counting from 1. If not set, it will
+     *        be defaulted to the first page.
+     * @param size Number of events to retrieve per page. If not set, it will be
+     *        defaulted to twenty.
+     *
+     * @return An {@code OK} HTTP response containing the list of events that
+     *         result from executing a search with the specified query in the
+     *         requested page.
+     *
+     * @throws IllegalArgumentException If either the page number is less than
+     *         one or the page size is less than zero.
+     */
+    @GET
+    @Path("search")
+    public Response seach(
+        @QueryParam("query") @DefaultValue("")   final String query,
+        @QueryParam("page")  @DefaultValue("1")  final int    page,
+        @QueryParam("size")  @DefaultValue("20") final int    size
+    ) throws IllegalArgumentException {
+        isTrue(page >= 1, "Page number must be greater than zero");
+        isTrue(size >= 0, "Page size must be non-negative");
+
+        final int start = (page - 1) * size;
+        return status(OK).entity(events.search(query, start, size)).build();
     }
 
 }
