9f2fb7a93446f98c59e81d4aedde01bf429441a3
Adrian <arfarinha@esei.uvigo.es>
2015-12-08 13:52:43 +0100

    Updates searchResults.xhtml and searchController.java to reflect the latest changes in the project
    A modification has been made upon the controller for the vent search functionality in order to redirect to the searchResults view from the index.Also,
    an event is pased now to the getIconFor() function so that has also been fixed.

--- jsf/src/main/java/es/uvigo/esei/dgss/letta/jsf/EventSearchController.java
+++ jsf/src/main/java/es/uvigo/esei/dgss/letta/jsf/EventSearchController.java
@@ -173,7 +173,7 @@ public class EventSearchController implements Serializable {
 		}
 		this.currentPage = pageIndex;
 
-		return "si";
+		return "searchResults.xhtml";
 	}
 
 	public String doSearchPrev() {
