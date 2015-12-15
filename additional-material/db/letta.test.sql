5a66e20f11f2b199903e2502a27b6200dd15bffc
Jesus Alvarez Casanova <jacasanova@esei.uvigo.es>
2015-12-15 09:43:44 +0100

    Fixes an error with test DB name
    Modifies the name of the database to letta-test.

--- additional-material/db/letta.test.sql
+++ additional-material/db/letta.test.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS `letta`;
+DROP DATABASE IF EXISTS `letta-test`;
 
-CREATE DATABASE `letta`;
-USE `letta`;
+CREATE DATABASE `letta-test`;
+USE `letta-test`;
 
-GRANT ALL PRIVILEGES ON letta.* TO 'letta'@'localhost' IDENTIFIED BY 'lettapass';
+GRANT ALL PRIVILEGES ON letta-test.* TO 'letta'@'localhost' IDENTIFIED BY 'lettapass';
 FLUSH PRIVILEGES;
 
 CREATE TABLE `Registration` (
