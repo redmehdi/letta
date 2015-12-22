798f5d38a698d6fbe0e85b92fc571de2fdd017ac
Jesus Alvarez Casanova <jacasanova@esei.uvigo.es>
2015-12-22 17:35:49 +0100

    Modifies EmailDuplicateException and LoginDuplicateException
    Removes the attribute message in both classes. Now both classes pass the
    exception message to the super();

--- service/src/main/java/es/uvigo/esei/dgss/letta/service/util/exceptions/EmailDuplicateException.java
+++ service/src/main/java/es/uvigo/esei/dgss/letta/service/util/exceptions/EmailDuplicateException.java
@@ -2,21 +2,9 @@ package es.uvigo.esei.dgss.letta.service.util.exceptions;
 
 public class EmailDuplicateException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private String message;
 
-	public EmailDuplicateException(String mes) {
-		this.message = mes;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
+	public EmailDuplicateException(String message) {
+		super(message);
 	}
 }
--- service/src/main/java/es/uvigo/esei/dgss/letta/service/util/exceptions/LoginDuplicateException.java
+++ service/src/main/java/es/uvigo/esei/dgss/letta/service/util/exceptions/LoginDuplicateException.java
@@ -2,21 +2,9 @@ package es.uvigo.esei.dgss.letta.service.util.exceptions;
 
 public class LoginDuplicateException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private String message;
 
-	public LoginDuplicateException(String mes) {
-		this.message = mes;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
+	public LoginDuplicateException(String message) {
+		super(message);
 	}
 }
