b52604ce6f39cd39c64f28e5ffdb2f6e347e30a3
mirkeet <abel.fernandez.nandin@gmail.com>
2016-06-27 19:11:29 +0200

    tsk100 Fix message list not having the proper style
    Modifies ListUserNotifications.java and messages.xhtml, and adds message.xhtml, so that the message list has
    the same styles as the rest of the application.

--- jsf/src/main/java/es/uvigo/esei/dgss/letta/jsf/ListUserNotifications.java
+++ jsf/src/main/java/es/uvigo/esei/dgss/letta/jsf/ListUserNotifications.java
@@ -1,19 +1,22 @@
 package es.uvigo.esei.dgss.letta.jsf;
 
 import java.io.IOException;
-
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-
 import javax.inject.Inject;
 
+import es.uvigo.esei.dgss.letta.domain.entities.Event.Category;
 import es.uvigo.esei.dgss.letta.domain.entities.User;
 import es.uvigo.esei.dgss.letta.domain.entities.UserNotifications;
+import es.uvigo.esei.dgss.letta.jsf.util.EventMappings;
 import es.uvigo.esei.dgss.letta.service.UserEJB;
 
 @ManagedBean(name = "userNotifications")
@@ -21,8 +24,44 @@ import es.uvigo.esei.dgss.letta.service.UserEJB;
 public class ListUserNotifications {
 	@Inject
 	private UserEJB userEJB;
-	
+
+	private int pageIndex=1;
+    private int pages=0;
+    private final int CTE_NUM_MESSAGES_PAGE=4;
+	private ArrayList<String> pagesLinks = new ArrayList<>();
+
+	public int getPages() {
+		return pages;
+	}
+	public void setPages(final int pages) {
+		this.pages = pages;
+	}
+	public int getPageIndex() {
+		return pageIndex;
+	}
+	public void setPageIndex(final int pageIndex) {
+		this.pageIndex = pageIndex;
+	}
+    public ArrayList<String> getPagesLinks() {
+		return pagesLinks;
+	}
+	public void setPagesLinks(final ArrayList<String> pagesLinks) {
+		this.pagesLinks = pagesLinks;
+	}
 	/**
+     * Initialize the number of pages in a paginated page.
+     *
+     */
+    @PostConstruct
+    public void init(){
+    	pages=getNotifications().size()/CTE_NUM_MESSAGES_PAGE;
+    	if (getNotifications().size()%CTE_NUM_MESSAGES_PAGE != 0)
+    		pages++;
+		for (int i = 0; i < pages; i++)
+            pagesLinks.add(String.valueOf(i + 1));
+    }
+
+    /**
 	 * Get a list of {@link UserNotifications}.
 	 * @return a sorted list of {@link UserNotifications}.
 	 */
@@ -31,6 +70,51 @@ public class ListUserNotifications {
 		Collections.sort(userNotifications, (c1, c2) -> c2.getNotificationId() - c1.getNotificationId());
 		
 		return  userNotifications;
+	}
+
+    /**
+     * Method to be called when the next command button is pressed
+     *
+     */
+    public void listUserNotificationsNext() {
+        if(this.pageIndex < this.pages )
+        	this.pageIndex++;
+    }
+
+    /**
+     * Method to be called when the previous command button is pressed
+     *
+     */
+    public void listUserNotificationsPrev(){
+		if (this.pageIndex > 1)
+			this.pageIndex--;
+	}
+
+    /**
+     * Returns a {@link String} representing the path to the corresponding
+     * {@link Category} icon image.
+     *
+     * @param eventType the {@link Category} to be translated into a icon path.
+     *
+     * @return a String with the path to the icon associated to the received
+     *         event type.
+     */
+    public String getIconFor(final Category eventType) {
+        return EventMappings.getIconFor(eventType);
+    }
+
+    /**
+     * Update the value of pageIndex and redirect.
+     *
+     * @param pageNumber, number of page to
+     *
+     */
+	public void jumpToPage(final String pageNumber) {
+		this.pageIndex = Integer.parseInt(pageNumber);
+	}
+
+	public boolean isAfter(final LocalDateTime d) {
+	    return d.isAfter(LocalDateTime.now());
 	}
 	
 	/**
