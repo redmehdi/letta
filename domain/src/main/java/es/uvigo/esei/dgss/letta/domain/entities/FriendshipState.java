1b9dd8614ec82bc5eee854ee7ee5552448d5dc8c
redouane mehdi <redouane@esei.uvigo.es>
2017-01-07 11:21:24 +0100

    tsk102 adds friendship state

---
+++ domain/src/main/java/es/uvigo/esei/dgss/letta/domain/entities/FriendshipState.java
@@ -0,0 +1,15 @@
+package es.uvigo.esei.dgss.letta.domain.entities;
+
+/**
+ * Constants that represent the friendship state in the system.
+ * @author world1mehdi
+ *
+ */
+
+public enum FriendshipState {
+	/**
+	 * The FriendshipState can be a regular pending, accepted, rejected or cancelled.
+	 */
+	PENDING,ACCEPTED,REJECTED,CANCELLED;
+	
+}
