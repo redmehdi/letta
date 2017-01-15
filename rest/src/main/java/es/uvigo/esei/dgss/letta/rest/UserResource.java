8d38af885c66511732c974d5df5510951cfc534b
redouane mehdi <redouane@esei.uvigo.es>
2017-01-15 22:59:52 +0100

    update

--- rest/src/main/java/es/uvigo/esei/dgss/letta/rest/UserResource.java
+++ rest/src/main/java/es/uvigo/esei/dgss/letta/rest/UserResource.java
@@ -1,9 +1,19 @@
 package es.uvigo.esei.dgss.letta.rest;
 
+import static javax.ws.rs.core.Response.status;
+import static javax.ws.rs.core.Response.Status.OK;
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+import static org.apache.commons.lang3.Validate.isTrue;
+
+import java.net.URI;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,16 +27,15 @@ import es.uvigo.esei.dgss.letta.domain.entities.Event;
 import es.uvigo.esei.dgss.letta.domain.entities.User;
 import es.uvigo.esei.dgss.letta.service.EventEJB;
 import es.uvigo.esei.dgss.letta.service.UserAuthorizationEJB;
-
-import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
-
-import static org.apache.commons.lang3.Validate.isTrue;
+import es.uvigo.esei.dgss.letta.service.UserEJB;
+import es.uvigo.esei.dgss.letta.service.util.exceptions.UserNotAuthorizedException;
 
 /**
  * Resource that represents the {@link User} private funcionalities
  *
  * @author Jesús Álvarez Casanova
  * @author Adolfo Álvarez López
+ * @author world1mehdi
  *
  */
 @Path("user")
@@ -39,6 +48,9 @@ public class UserResource {
 
 	@Inject
 	private EventEJB eventEJB;
+	
+	@Inject
+	private UserEJB userEJB;
 
 	@Inject
 	private UserAuthorizationEJB auth;
@@ -59,6 +71,7 @@ public class UserResource {
 	 *         response with no content if the login doesn't match with the
 	 *         logged user
 	 */
+	
 	@GET
 	@Path("{login}/joined")
 	public Response getEventsJoinedByUser(
@@ -90,6 +103,139 @@ public class UserResource {
 			@PathParam("login") final String userLogin) {
 		if (userLogin.equals(auth.getCurrentUser().getLogin()))
             return Response.ok(eventEJB.getEventsOwnedByCurrentUser()).build();
+        else
+            return Response.status(UNAUTHORIZED).build();
+	}
+	
+	/**
+	 * Sends friend requests
+	 * 
+	 * @param userLogin
+	 * 				the login if {@link User}
+	 * @param friendLogin
+	 * 				the login of friend's {@link User}
+	 * @return An {@code OK} HTTP response
+	 * @throws IllegalArgumentException
+	 * @throws SecurityException
+	 */
+	@POST
+	@Path("{login}/sendRequest/{friendLogin}")
+    public Response sendFriendRequest(
+    		@PathParam("login") final String userLogin,
+    		@PathParam("friendLogin") final String friendLogin
+    ) throws IllegalArgumentException, SecurityException {
+		if (userLogin.equals(auth.getCurrentUser().getLogin())){
+			userEJB.sendRequest(friendLogin);
+            return Response.status(OK).build();
+		} else
+            return Response.status(UNAUTHORIZED).build();
+    }
+	
+	/**
+	 * Cancels friend requests
+	 * 
+	 * @param userLogin
+	 * 				the login if {@link User}
+	 * @param friendLogin
+	 * 				the login of friend's {@link User}
+	 * @return An {@code OK} HTTP response
+	 * @throws IllegalArgumentException
+	 * @throws SecurityException
+	 */
+	@POST
+	@Path("{login}/cancelRequest/{friendLogin}")
+    public Response cancelFriendRequest(
+    		@PathParam("login") final String userLogin,
+    		@PathParam("friendLogin") final String friendLogin
+    ) throws IllegalArgumentException, SecurityException {
+		if (userLogin.equals(auth.getCurrentUser().getLogin())){
+			userEJB.cancelFriendship(friendLogin);
+            return Response.status(OK).build();
+		} else
+            return Response.status(UNAUTHORIZED).build();
+    }
+	
+	/**
+	 * Accepts friend requests
+	 * 
+	 * @param userLogin
+	 * 				the login if {@link User}
+	 * @param friendLogin
+	 * 				the login of friend's {@link User}
+	 * @return An {@code OK} HTTP response
+	 * @throws IllegalArgumentException
+	 * @throws SecurityException
+	 */
+	
+	@PUT
+	@Path("{login}/acceptRequest/{friendLogin}")
+    public Response acceptFriendRequest(
+    		@PathParam("login") final String userLogin,
+    		@PathParam("friendLogin") final String friendLogin
+    ) throws IllegalArgumentException, SecurityException {
+		if (userLogin.equals(auth.getCurrentUser().getLogin())){
+			userEJB.acceptOrRejectFriendRequest(userLogin, true);
+            return Response.status(OK).build();
+		} else
+            return Response.status(UNAUTHORIZED).build();
+    }
+	
+	/**
+	 * Rejects friend requests
+	 * 
+	 * @param userLogin
+	 * 				the login if {@link User}
+	 * @param friendLogin
+	 * 				the login of friend's {@link User}
+	 * @return An {@code OK} HTTP response
+	 * @throws IllegalArgumentException
+	 * @throws SecurityException
+	 */
+	@PUT
+	@Path("{login}/rejectRequest/{friendLogin}")
+    public Response rejectFriendRequest(
+    		@PathParam("login") final String userLogin,
+    		@PathParam("friendLogin") final String friendLogin
+    ) throws IllegalArgumentException, SecurityException {
+		if (userLogin.equals(auth.getCurrentUser().getLogin())){
+			userEJB.acceptOrRejectFriendRequest(friendLogin, false);
+            return Response.status(OK).build();
+		} else
+            return Response.status(UNAUTHORIZED).build();
+    }
+	
+	/**
+	 * Remove friendship
+	 * 
+	 * @param userLogin
+	 * 				the login if {@link User}
+	 * @param friendLogin
+	 * 				the login of friend's {@link User}
+	 * @return An {@code OK} HTTP response
+	 * @throws IllegalArgumentException
+	 * @throws SecurityException
+	 */
+	@DELETE
+	@Path("{login}/removeFriendship/{friendLogin}")
+    public Response removeFriendship(
+    		@PathParam("login") final String userLogin,
+    		@PathParam("friendLogin") final String friendLogin
+    ) throws IllegalArgumentException, SecurityException {
+		if (userLogin.equals(auth.getCurrentUser().getLogin())){
+			userEJB.removeFriendship(friendLogin);
+            return Response.status(OK).build();
+		} else
+            return Response.status(UNAUTHORIZED).build();
+    }
+	
+	
+	@GET
+	@Path("{login}/searchUser/{keyword}")
+	public Response searchForAnUser(
+			@PathParam("login") final String userLogin,
+			@PathParam("keyword") final String keyword) {
+		if (userLogin.equals(auth.getCurrentUser().getLogin()))
+            return Response.ok(userEJB.searchUser(keyword)).build();
         else
             return Response.status(UNAUTHORIZED).build();
 	}
