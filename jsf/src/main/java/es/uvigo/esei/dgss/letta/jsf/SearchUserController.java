d77010c05618d98fa0c58bc34f1b7318581526d2
redouane mehdi <redouane@esei.uvigo.es>
2017-06-05 00:01:49 +0200

    task105 adds controller for search users

--- jsf/src/main/java/es/uvigo/esei/dgss/letta/jsf/SearchUserController.java
+++ jsf/src/main/java/es/uvigo/esei/dgss/letta/jsf/SearchUserController.java
@@ -1,5 +1,7 @@
 package es.uvigo.esei.dgss.letta.jsf;
 
+import java.io.Serializable;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,60 +11,72 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
-import es.uvigo.esei.dgss.letta.domain.entities.State;
-import es.uvigo.esei.dgss.letta.domain.entities.User;
+import es.uvigo.esei.dgss.letta.bean.UserBean;
 import es.uvigo.esei.dgss.letta.service.UserEJB;
+import es.uvigo.esei.dgss.letta.transform.UserTransform;
 
 @ViewScoped
 @ManagedBean(name = "searchUserController")
-public class SearchUserController {
+public class SearchUserController extends UserTransform implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1878250668939516230L;
 	private int pageIndex = 0;
 	private int pages = 0;
 	private int currentPage = 1;
-
 	private List<String> pagesLinks = new ArrayList<>();
+	public int getPageIndex() {
+		return pageIndex;
+	}
+
+	public void setPageIndex(int pageIndex) {
+		this.pageIndex = pageIndex;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
 	
 	private String keyword;
 	
 	@Inject 
 	private UserEJB userEJB;
 	
-	private List<User> users;
+	@Inject Principal currentUserPrincipal;
+
+	private List<UserBean> users = new ArrayList<>();
 	
-	private String name;
-	
-	private String country;
-	
-	private byte[] photo;
-	
+
 	@PostConstruct
-	public void init() { 
-		
+	public void init() {
+
 		FacesContext facesContext = FacesContext.getCurrentInstance();
-		if (facesContext.getExternalContext().getRequestParameterMap()
-				.get("keyword") != null) {
-			String srch_terms = (String) facesContext.getExternalContext()
-					.getRequestParameterMap().get("keyword");
+		if (facesContext.getExternalContext().getRequestParameterMap().get("keyword") != null) {
+			String keyword_term = (String) facesContext.getExternalContext().getRequestParameterMap()
+					.get("keyword");
 
-			int page = Integer.parseInt(facesContext.getExternalContext()
-					.getRequestParameterMap().get("page"));
-			
-	}
-		
-		if(this.keyword != null) {
-			
-			this.users = userEJB.searchUser(keyword);
-			
+			if(keyword_term != null && !keyword_term.equals("")) {
+				this.keyword = keyword_term;
+			}
+			this.users = transformUsers(userEJB.searchUser(keyword), this.currentUserPrincipal.getName());
 		}
-	}
 
-	public List<User> getUsers() {
-		return users;
-	}
 
-	public void setUsers(List<User> users) {
-		this.users = users;
 	}
 
 	public String getKeyword() {
@@ -73,6 +87,26 @@ public class SearchUserController {
 		this.keyword = keyword;
 	}
 	
+	public List<UserBean> getUsers() {
+		return users;
+	}
+	
+	public void setUsers(List<UserBean> userBeans) {
+		this.users = userBeans;
+	}
+	
+	public List<UserBean> getUser(){
+		
+		return transformUsers(userEJB.searchUser(keyword), this.currentUserPrincipal.getName());
+	}
+
+	public List<String> getPagesLinks() {
+		return pagesLinks;
+	}
+
+	public void setPagesLinks(List<String> pagesLinks) {
+		this.pagesLinks = pagesLinks;
+	}
 	
 
 }
